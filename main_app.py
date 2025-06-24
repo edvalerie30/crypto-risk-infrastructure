d5080a065c1f0277baad18fdaee09425c00ea41e
EC2 Default User <ec2-user@ip-172-31-45-45.us-east-2.compute.internal>
2025-06-24 15:40:27 +0000

    Fix: moved and added main_app.py

---
+++ main_app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Crypto Risk Dashboard", layout="wide")
+st.title("✅ App Is Running from main_app.py")
+st.write("It’s rendering from the renamed file.")
